fd1cee32551ff995ad322352cbed8cc2e4be5922
Nika B <78375163+Nika-Bushinova@users.noreply.github.com>
2022-07-20 07:42:36 -0400

    fix:update the function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-module.exports = function check(str, bracketsConfig) {
-
-    let brackets = bracketsConfig.join('').replace(/,/g, '');
-        let stack = [];
-    for (let bracket of str) {
-            let bracketsIndex = brackets.indexOf(bracket)
-
-            if (bracketsIndex % 2 === 0) {
-                stack.push(bracketsIndex)
-                if (bracket === brackets[bracketsIndex+1]){
-                    stack.pop();
-                }
-
+module.exports = function check(str, bracketsConfig){
+  let brackets = bracketsConfig.join('').replace(/,/g, '').split('')
+  let openBrackets=[]
+  let closeBrackets=[]
+    brackets.forEach((element,index)=>{
+    index%2===0?openBrackets.push(element):openBrackets
+      index%2!==0?closeBrackets.push(element):closeBrackets
+  })
+  let brackets_pair={}
+    let bracketsObj = Object.fromEntries(bracketsConfig)
+ Object.entries(bracketsObj).forEach(([key, value]) => {
+  brackets_pair[value] = key
+})
+  let stack=[]
+  for(let i=0;i<str.length;i++){
+    let curr=str[i]
+        let topEl=stack[stack.length-1]
+    if(openBrackets.includes(curr)){
+if(closeBrackets.includes(curr)&&curr===topEl){
+  stack.pop()
+}else{
+       stack.push(curr)
+}
+    }else{
+            if(stack.length===0){
+            return false//
             }
-            else {
-                if (stack.pop() !== bracketsIndex-1){
-                    return false;
-                }
-            }
-
-        }
-        return stack.length === 0
-
+      if(brackets_pair [curr]==topEl){
+  stack.pop()
+       
+      }else{
+  return false
+}      
+  }
+  }
+  return stack.length===0
 }
